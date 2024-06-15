70e11e9f956266b6c7f30c1e84c6fd1d090450c8
ACOUNTBOOK.ERP <94230977+EduarCha@users.noreply.github.com>
2024-06-14 20:59:40 -0500

    okv

--- Login/cajero_dashboard/script.js
+++ Login/cajero_dashboard/script.js
@@ -2,7 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
     const menuItems = document.querySelectorAll('.menu-item');
     const logo = document.getElementById('logo');
     const modal = document.getElementById('accountModal');
+    const productModal = document.getElementById('productModal');
     const span = document.getElementsByClassName('close')[0];
+    const spanProduct = document.getElementsByClassName('close-product')[0];
+    const cancelButton = document.getElementById('cancelButton');
+    const addProductIcon = document.getElementById('addProductIcon');
 
     menuItems.forEach(item => {
         item.addEventListener('click', function() {
@@ -24,9 +28,23 @@ document.addEventListener("DOMContentLoaded", function() {
             modal.style.display = "none";
         }
     });
+
+    addProductIcon.addEventListener('click', function() {
+        productModal.style.display = "block";
+    });
+
+    spanProduct.addEventListener('click', function() {
+        productModal.style.display = "none";
+    });
+
+    cancelButton.addEventListener('click', function() {
+        productModal.style.display = "none";
+    });
+
+    calculateTax();
 });
 
-let totalAmount = 20000; // Ejemplo de total, debería calcular esto dinámicamente.
+let totalAmount = 30000; // Ejemplo de total, debería calcular esto dinámicamente.
 let cashReceived = 0;
 
 function addCash(amount) {
@@ -45,3 +63,24 @@ function calculateChange() {
     document.getElementById('change').textContent = `S/. ${change.toFixed(2)}`;
 }
 
+function calculateTax() {
+    // Obtener el valor total como texto
+    let totalText = document.getElementById('total_id').innerText;
+
+    // Eliminar el prefijo "S/." y convertir a número
+    let total = parseFloat(totalText.replace('S/.', '').replace(/\./g, '').replace(',', '.'));
+
+    // Calcular el IVA (19%)
+    let taxRate = 0.19;
+    let tax = total * taxRate;
+
+    // Calcular el subtotal
+    let subtotal = total - tax;
+
+    // Actualizar los valores en el HTML
+    document.getElementById('subtotal_id').innerText = `S/. ${subtotal.toFixed(2)}`;
+    document.getElementById('tax_id').innerText = `S/. ${tax.toFixed(2)}`;
+}
+
+// Llamar a la función calculateTax al cargar la página
+document.addEventListener('DOMContentLoaded', calculateTax);
