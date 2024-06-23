024e08ad42fe3eb2490a45a7aa6c0b69979bece5
ACOUNTBOOK.ERP <94230977+EduarCha@users.noreply.github.com>
2024-06-22 19:50:51 -0500

    sidebar r
    carrito para productos

--- Dashboard/script/pos.js
+++ Dashboard/script/pos.js
@@ -1,4 +1,15 @@
-function addProducto(){
-    alert('roll 1');
-    console.log("clik add producto")
+// pos.js
+export function cargarProductos() {
+    return fetch('./docs/productos.json')  // Ajuste de ruta
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => data)
+        .catch(error => {
+            console.error('Error loading products:', error);
+            return [];  // Devolver un array vacío u otro valor por defecto en caso de error
+        });
 }
